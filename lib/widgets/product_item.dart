a4b73df3c93a695cf3a6f98ac727df71af439ee8
TapanDangi <tapandangi17@gmail.com>
2023-01-02 22:42:57 +0530

    02/01 commit

---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class ProductItem extends StatelessWidget {
+  const ProductItem({
+    Key? key,
+    required this.id,
+    required this.title,
+    required this.imageUrl,
+  }) : super(key: key);
+
+  final String id;
+  final String title;
+  final String imageUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      //forces the child widget to wrap into a certain shape
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        footer: GridTileBar(
+          title: Text(
+            title,
+            textAlign: TextAlign.center,
+          ),
+          backgroundColor: Colors.black87,
+          leading: IconButton(
+            icon: const Icon(
+              Icons.favorite,
+            ),
+            onPressed: () {},
+            color: Theme.of(context).colorScheme.secondary,
+          ),
+          trailing: IconButton(
+            icon: const Icon(
+              Icons.shopping_cart,
+            ),
+            onPressed: () {},
+            color: Theme.of(context).colorScheme.secondary,
+          ),
+        ),
+        child: Image.network(
+          imageUrl,
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
