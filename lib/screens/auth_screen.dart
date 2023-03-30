f065c19f97533a21ec5e7d50a149e985414f700c
TapanDangi <tapandangi17@gmail.com>
2023-03-30 22:47:15 +0530

    30/3 commit

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -101,7 +101,10 @@ class AuthCard extends StatefulWidget {
   State<AuthCard> createState() => _AuthCardState();
 }
 
-class _AuthCardState extends State<AuthCard> {
+class _AuthCardState extends State<AuthCard>
+    with SingleTickerProviderStateMixin {
+  //SingleTickerProviderStateMixin is used by vsync whether it is currently visible.
+  //It lets our widget know when a frame update is due to smoothly play animations.
   final GlobalKey<FormState> _formKey = GlobalKey();
   AuthMode _authMode = AuthMode.login;
   final Map<String, String> _authData = {
@@ -110,6 +113,46 @@ class _AuthCardState extends State<AuthCard> {
   };
   var _isLoading = false;
   final _passwordController = TextEditingController();
+  late AnimationController _controller;
+  late Animation<Offset> _slideAnimation;
+  late Animation<double> _opacityAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      //this refers to the current class _AuthCardState.
+      duration: const Duration(milliseconds: 300),
+    );
+    //vsync is an argument in which we give AnimationController a pointer at an object/widget
+    //which it should watch. Only when the widget is re-visible on the screen, the animation should play.
+    //This optimizes performance since only widget visible to the user are animated.
+    _slideAnimation = Tween<Offset>(
+      //Tween class gives an object which knows how to animate between two values.
+      begin: const Offset(0, -1.5),
+      end: const Offset(0, 0),
+    ).animate(
+      //Tween itself does not give an animation but only gives information of how to animate.
+      //we create an animate object based on this and pass an animate object that wraps around Tween
+      //that tells how to animate it.
+      CurvedAnimation(parent: _controller, curve: Curves.fastOutSlowIn),
+    );
+    _opacityAnimation = Tween(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeIn));
+
+    //_heightAnimation.addListener(() => setState(() {}));
+  }
+  //both AnimationController and Animation should be configured when State object is created
+  //so initstate is used.
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
 
   void _showErrorDialog(String message) {
     showDialog(
@@ -183,10 +226,14 @@ class _AuthCardState extends State<AuthCard> {
       setState(() {
         _authMode = AuthMode.signup;
       });
+      _controller.forward();
+      //forward method starts the animation.
     } else {
       setState(() {
         _authMode = AuthMode.login;
       });
+      _controller.reverse();
+      //reverse method reverses the animation.
     }
   }
 
@@ -198,13 +245,21 @@ class _AuthCardState extends State<AuthCard> {
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: Container(
+      child: AnimatedContainer(
+        //AnimatedContainer is useful for simple animation like changing the height or width.
+        //It is recommended for animating a container as it does all the heavy lifting itself.
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeIn,
         height: _authMode == AuthMode.signup ? 320 : 260,
-        constraints:
-            BoxConstraints(minHeight: _authMode == AuthMode.signup ? 320 : 260),
+        constraints: BoxConstraints(
+          minHeight: _authMode == AuthMode.signup ? 320 : 260,
+        ),
         width: deviceSize.width * 0.75,
         padding: const EdgeInsets.all(16.0),
         child: Form(
+          //child argument is a part of the widget tree which actually won't be rebuilt on rebuilding the builder.
+          //Form and its constituents does not change when we animate the container, so it is passed to chld argument.
+          //This child argument is made available on the ch argument of the builder function.
           key: _formKey,
           child: SingleChildScrollView(
             child: Column(
@@ -236,21 +291,34 @@ class _AuthCardState extends State<AuthCard> {
                     _authData['password'] = value!;
                   },
                 ),
-                if (_authMode == AuthMode.signup)
-                  TextFormField(
-                    enabled: _authMode == AuthMode.signup,
-                    decoration:
-                        const InputDecoration(labelText: 'Confirm Password'),
-                    obscureText: true,
-                    validator: _authMode == AuthMode.signup
-                        ? (value) {
-                            if (value != _passwordController.text) {
-                              return 'Passwords do not match!';
-                            }
-                            return null;
-                          }
-                        : null,
+                AnimatedContainer(
+                  duration: const Duration(milliseconds: 300),
+                  constraints: BoxConstraints(
+                    minHeight: _authMode == AuthMode.signup ? 60 : 0,
+                    maxHeight: _authMode == AuthMode.signup ? 120 : 0,
                   ),
+                  curve: Curves.easeIn,
+                  child: FadeTransition(
+                    opacity: _opacityAnimation,
+                    child: SlideTransition(
+                      position: _slideAnimation,
+                      child: TextFormField(
+                        enabled: _authMode == AuthMode.signup,
+                        decoration: const InputDecoration(
+                            labelText: 'Confirm Password'),
+                        obscureText: true,
+                        validator: _authMode == AuthMode.signup
+                            ? (value) {
+                                if (value != _passwordController.text) {
+                                  return 'Passwords do not match!';
+                                }
+                                return null;
+                              }
+                            : null,
+                      ),
+                    ),
+                  ),
+                ),
                 const SizedBox(
                   height: 20,
                 ),
